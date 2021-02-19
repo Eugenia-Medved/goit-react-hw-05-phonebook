7e92265c2bd52e3b6560e82cd1e0af6f4b5898be
YevheniiaBondarenko <65921369+YevheniiaBondarenko@users.noreply.github.com>
2021-02-19 12:33:36 +0200

    fix blic on phone list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,14 +8,15 @@ import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList';
 import Alert from 'components/Alert/Alert';
 
+const contactsReserv = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     message: false,
     showAlert: false,
@@ -26,8 +27,9 @@ class App extends Component {
     const parsedContacts = JSON.parse(contacts);
 
     if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+      return this.setState({ contacts: parsedContacts });
     }
+    this.setState({ contacts: contactsReserv });
   }
 
   componentDidUpdate(prevProps, prevState) {
